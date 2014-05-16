0cf00b7faeb84b2e4e32528c3417663dbcc7211c
Denis Pismenny <denis.pismenny@gmail.com>
2014-05-16 17:37:17 +0300

    magnific-popup failed

--- js/common.js
+++ js/common.js
@@ -9,4 +9,20 @@ head.ready(function() {
 		sliderHeight();
 	});
 
+	$('.popup-with-zoom-anim').magnificPopup({
+			type: 'inline',
+
+			fixedContentPos: false,
+			fixedBgPos: true,
+
+			overflowY: 'auto',
+
+			closeBtnInside: true,
+			preloader: false,
+
+			midClick: true,
+			removalDelay: 300,
+			mainClass: 'my-mfp-zoom-in'
+		});
+
 });
