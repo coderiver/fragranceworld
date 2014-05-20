76c444163991f2c4896127ea6bdebb0b87b05440
Denis Pismenny <denis.pismenny@gmail.com>
2014-05-21 00:40:34 +0300

    add new slides

--- js/common.js
+++ js/common.js
@@ -1,61 +1,82 @@
 head.ready(function() {
 
-	function load(time){
-		var x = new XMLHttpRequest();
-		x.open('GET', "http://localhost:5646/walter/" + time, true);
-		x.send();
-	}
-
-	load(20);
-	load(100);
-	load(500);
-	load(2000);
-	load(3000);
-
-	setTimeout(function(){
-		Pace.ignore(function(){
-			load(3100);
-		});
-	}, 4000);
-
-	Pace.on('hide', function(){
-		console.log('done');
-	});
-
-	function sliderHeight(){
-		var el_height = $(window).height();
-		$('.js-slider').height(el_height);
-	} sliderHeight();
-
-	$(window).resize(function() {
-		sliderHeight();
-	});
-
 	function popup() {
 		$(' .btn-popup ').on('click', function(event) {
 			var id = $(this).attr("href");
-			$(' .popup-wrap ').addClass(' is-open ');
+			$('.popup-wrap').addClass(' is-open ');
 			$(id).addClass('is-open');
-			// $(' .popup ').removeClass('is-open');
-			// $(' .overlay ').addClass(' is-visible ');
-			$(' .overlay ').fadeIn('fast');
-			$(' body ').addClass(' has-popup ');
+			$('.overlay').fadeIn('fast');
+			$('body').addClass(' has-popup ');
 			return false;
 		});
-		$('.popup').on('click', function() {
+		$(' .popup ').on('click', function() {
 			event.stopPropagation();
 		});
 		$(' .popup__close, .popup-wrap ').on('click', function() {
-			$(' .popup-wrap ').removeClass(' is-open ');
-			// $(' .overlay ').removeClass(' is-visible ');
-			$(' .overlay ').fadeOut('fast');
-			$(' body ').removeClass(' has-popup ');
-			$(' .popup ').removeClass('is-open');
+			$('.popup-wrap').removeClass(' is-open ');
+			$('.overlay').fadeOut('fast');
+			$('body').removeClass(' has-popup ');
+			$('.popup').removeClass('is-open');
 			return false;
 		});
 	} popup();
 
+
+	function slider(){
+
+		// line-height for vertical-align
+		var el_height = $('.slider__el').height();
+		$('.slider__el').css('line-height', el_height + 'px');
+
+		// slider paginator
+		$(' .slider__paginator a ').click(function (e){
+			e.preventDefault();
+
+			var target = this.hash,
+					$target = $(target);
+
+			$('html, body').stop().animate({
+				'scrollTop': $target.offset().top
+			}, 500, 'swing', function () {
+					window.location.hash = target;
+			});
+		});
+
+		// add 'is-active' class to the first pagination
+		$('.slider__paginator li').first().addClass('is-active');
+
+		// switch 'is-active' class for paginator on scroll
+		function sliderScroll(){
+			$(' .slider__el ').each(function(){
+				var pos = $(this).offset().top;
+				var id = $(this).attr('id');
+				if( $(window).scrollTop() >= (pos)){
+					// $(this).addClass('is-animated');
+					$('.slider__paginator li').removeClass('is-active');
+					$('[href = #'+id+']').parent().addClass('is-active');
+				}
+			});
+		}
+
+		$(window).scroll(function() {
+			sliderScroll();
+		});
+
+		// slider prev/next navigation
+		$(' .slider__nav a ').click(function (e){
+			e.preventDefault();
+
+			var target = this.hash,
+					$target = $(target);
+
+			$('html, body').stop().animate({
+				'scrollTop': $target.offset().top
+			}, 500, 'swing', function () {
+					window.location.hash = target;
+			});
+		});
+
+	} slider();
+
+
 });
-// $(window).load(function() {
-	// $('.slider__pic_blured').addClass('is-active');
-// });
--- js/init.js
+++ js/init.js
@@ -1,5 +1,6 @@
 head.load(
 	"js/lib/jquery.js",
 	"js/lib/pace.min.js",
+	"js/lib/jquery.mousewheel.js",
 	"js/common.js"
 );
